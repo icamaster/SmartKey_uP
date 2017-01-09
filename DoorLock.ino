efea36696552214dfd1b6aa08b64a038d61a2d3d
Cezar Chirila <mini_cezarus@yahoo.com>
2017-01-09 21:28:46 +0200

    Add files via upload

---
+++ DoorLock.ino
@@ -0,0 +1,30 @@
+#include <Servo.h> 
+
+Servo myservo;
+
+void setup() 
+{ 
+  Serial.begin(9600);
+} 
+
+void loop() {
+    if (Serial.available() > 0) {
+      String input = Serial.readString();
+        toggleDoor(input);
+    }
+}
+
+void toggleDoor(String input){
+  myservo.attach(9);
+
+  if(input.equals("open")){
+    myservo.write(135); // Unlock door
+  }
+  else {
+    myservo.write(45);  // Lock door
+  }
+    delay(1000);
+    myservo.detach();
+    Serial.println("ok");
+}
+
